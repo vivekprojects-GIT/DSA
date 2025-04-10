ae8f7257777d9d042340f4147a0e8a7a5ccc9fb5
vivekprojects-GIT <vivekuni7@gmail.com>
2025-04-09 20:19:58 -0500

    count_ones_challenge.py

---
+++ count_ones_challenge.py
@@ -0,0 +1,9 @@
+# 💡 Bonus: Count how many times 1 appears
+boxes = [1, 4, 1, 7, 1, 0, 2, 1]
+count = 0
+
+for num in boxes:
+    if num == 1:
+        count += 1
+
+print("🧮 Total number of 1s:", count)
